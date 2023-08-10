41a7c98fadaf5280b0b3adb0f051f70f8600bd5b
Ashish Sharda <ashishjsharda@gmail.com>
2023-08-10 10:03:36 -0500

    first commit

---
+++ src/main/java/com/example/springrediskafkademo/SpringRedisKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrediskafkademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisKafkaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisKafkaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springrediskafkademo/SpringRedisKafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrediskafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisKafkaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
